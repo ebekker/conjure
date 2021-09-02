ec0cf72740c1f8ee6d5601634a507d620074be43
Eugene Bekker <ebekker@users.noreply.github.com>
2021-09-02 09:48:01 -0400

    sampling Blazor-native sign in/out

---
+++ docs/examples/Conjure.BlazorAuth.Sample1/Pages/Identity/SignIn.razor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.JSInterop;
+using System.Text.Json;
+
+namespace Conjure.BlazorAuth.Sample1.Pages.Identity
+{
+    public partial class SignIn
+    {
+        [Inject]
+        private IHttpContextAccessor HttpContextAccessor { get; set; } = default!;
+
+        [Inject]
+        private NavigationManager Nav { get; set; } = default!;
+
+        [Inject]
+        private ISignInClient SignInClient { get; set; } = default!;
+
+        [Inject]
+        private IJSRuntime JS { get; set; } = default!;
+
+        [Inject]
+        private AuthenticationStateProvider AuthStateProvider { get; set; } = default!;
+
+        [Inject]
+        private SignInManager<IdentityUser> SIM { get; set; } = default!;
+
+        private AuthenticationState? _authState;
+
+        private string? _result;
+
+        public string? UserName { get; set; } = "jdoe1@mailinator.com";
+        public string? Password { get; set; } = "P@$$w0rd";
+
+        public bool RememberMe { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                _authState = await AuthStateProvider.GetAuthenticationStateAsync();
+                StateHasChanged();
+            }
+        }
+
+        private async Task DoSignIn()
+        {
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password))
+            {
+                _result = "You must specify a username + password";
+                return;
+            }
+
+            var result = await SignInClient.SignInAsync(UserName, Password);
+
+            if (result == null)
+            {
+                _result = "No response";
+            }
+            else
+            {
+                _result = JsonSerializer.Serialize(result);
+            }
+
+            if (result?.Succeeded ?? false)
+            {
+                _authState = await AuthStateProvider.GetAuthenticationStateAsync();
+                StateHasChanged();
+
+                if (AuthStateProvider is IHostEnvironmentAuthenticationStateProvider heasp)
+                {
+                    _authState = new AuthenticationState(await SignInClient.CreateClaimsPrincipal(UserName));
+                    heasp.SetAuthenticationState(Task.FromResult(_authState));
+                }
+            }
+        }
+    }
+}
---
+++ docs/examples/Conjure.BlazorAuth.Sample1/Pages/Identity/SignOut.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+
+namespace Conjure.BlazorAuth.Sample1.Pages.Identity
+{
+    public partial class SignOut
+    {
+        [Inject]
+        private NavigationManager Nav { get; set; } = default!;
+
+        [Inject]
+        private ISignInClient SignInClient { get; set; } = default!;
+
+        [Inject]
+        private IJSRuntime JS { get; set; } = default!;
+
+        [Inject]
+        private AuthenticationStateProvider AuthStateProvider { get; set; } = default!;
+
+        private AuthenticationState? _authState;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                _authState = await AuthStateProvider.GetAuthenticationStateAsync();
+                StateHasChanged();
+            }
+        }
+
+        private async Task DoSignOut()
+        {
+            if (await SignInClient.SignOutAsync())
+            {
+                _authState = await AuthStateProvider.GetAuthenticationStateAsync();
+                StateHasChanged();
+
+                if (AuthStateProvider is IHostEnvironmentAuthenticationStateProvider heasp)
+                {
+                    _authState = new AuthenticationState(await SignInClient.CreateClaimsPrincipal());
+                    heasp.SetAuthenticationState(Task.FromResult(_authState));
+                }
+
+                //Nav.NavigateTo("/", forceLoad: false, replace: false);
+                return;
+            }
+            await JS.InvokeVoidAsync("alert", "Unable to sign out.");
+        }
+
+        private Task DoCancel()
+        {
+            Nav.NavigateTo("/", forceLoad: false, replace: false);
+            return Task.CompletedTask;
+        }
+    }
+}
