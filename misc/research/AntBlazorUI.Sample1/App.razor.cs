1376685a9f67914689d276abd1862f6e60f64c03
Eugene Bekker <ebekker@users.noreply.github.com>
2021-10-16 21:20:19 -0400

    experimenting with AntBlazor

---
+++ misc/research/AntBlazorUI.Sample1/App.razor.cs
@@ -0,0 +1,40 @@
+﻿using Conjure.BlazorKit;
+
+namespace AntBlazorUI.Sample1;
+
+public partial class App
+{
+    public static NavMenuGroup Menu =>
+        new()
+        {
+            Children = new NavMenuItem[]
+            {
+                new ScreenMenuItem<Pages.Test1>
+                {
+                    Icon = "Layout",
+                },
+
+                new NavMenuGroup
+                {
+                    Icon = "layout",
+                    Label = "Bunch of Pages",
+                    Children = new NavMenuItem[]
+                    {
+                        new ScreenMenuItem<Pages.DummyPages.Page1>(),
+                        new ScreenMenuItem<Pages.DummyPages.Page2>(),
+                    },
+                },
+
+                new NavMenuGroup
+                {
+                    Icon = "idcard",
+                    Label = "Bunch More",
+                    Children = new NavMenuItem[]
+                    {
+                        new ScreenMenuItem<Pages.DummyPages.Page3>(),
+                        new ScreenMenuItem<Pages.DummyPages.Page4>(),
+                    },
+                },
+            },
+        };
+}
---
+++ misc/research/AntBlazorUI.Sample1/README.md
@@ -0,0 +1,1 @@
+# AntBlazorUI.Sample1
