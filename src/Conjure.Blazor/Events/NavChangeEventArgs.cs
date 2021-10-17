9f8657212b908bd63a0f7c15325968c35acf2406
Eugene Bekker <ebekker@users.noreply.github.com>
2021-10-16 21:22:34 -0400

    adding basic events and context

---
+++ src/Conjure.Blazor/Events/NavChangeEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace Conjure.Blazor.Events;
+
+public class NavChangeEventArgs
+{
+    public string? NewUri { get; init; }
+
+    public Func<Task<bool>>? Cancel { get; set; }
+}
---
+++ src/Conjure.Blazor/IAppContext.cs
@@ -0,0 +1,13 @@
+﻿using Conjure.Blazor.Events;
+
+namespace Conjure.Blazor;
+public interface IAppContext
+{
+    System.Security.Claims.ClaimsPrincipal User { get; }
+
+    bool HasPermission(string perm);
+
+    Task ShowMessage(string message);
+
+    EventHandler<NavChangeEventArgs> NavChange { get; set; }
+}
