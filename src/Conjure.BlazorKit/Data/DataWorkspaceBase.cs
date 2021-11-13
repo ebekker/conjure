84e745c85efc56efa2b562792682f1a0865b24e1
Eugene Bekker <ebekker@users.noreply.github.com>
2021-11-13 10:02:22 -0500

    baseline for DB contexts and aggregate workspace

---
+++ src/Conjure.BlazorKit/Data/DataWorkspaceBase.cs
@@ -0,0 +1,88 @@
+﻿// Conjure application framework.
+// Copyright (C) Conjure.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Conjure.BlazorKit.Data;
+
+public class DataWorkspaceBase : IDisposable
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly IServiceScope _scope;
+    private readonly List<DbInfo> _dbContexts = new();
+    private bool _disposedValue;
+
+    public DataWorkspaceBase(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+        _scope = _scopeFactory.CreateScope();
+        Console.Error.WriteLine("Creating WORKSPACE: " + GetHashCode());
+    }
+
+    protected TDbContext CreateContext<TDbContext>()
+        where TDbContext : DbContextBase
+    {
+        return _scope.ServiceProvider.GetRequiredService<TDbContext>();
+    }
+
+    protected void AddContext(DbContextBase db, Action? onDispose = null)
+    {
+        _dbContexts.Add(new()
+        {
+            _db = db,
+            _onDispose = onDispose,
+        });
+    }
+
+    protected int CountContexts => _dbContexts.Count;
+
+    protected IEnumerable<DbContextBase> AllContexts => _dbContexts.Select(x => x._db!);
+
+    protected void DisposeContexts()
+    {
+        foreach (var context in _dbContexts)
+        {
+            context._db?.Dispose();
+            context._onDispose?.Invoke();
+        }
+        _dbContexts.Clear();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+                Console.Error.WriteLine("Disposing WORKSPACE: " + GetHashCode());
+                DisposeContexts();
+                _scope.Dispose();
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~DataWorkspaceBase()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    private class DbInfo
+    {
+        public DbContextBase? _db;
+        public Action? _onDispose;
+    }
+}
---
+++ src/Conjure.BlazorKit/Data/DbContextBase.cs
@@ -0,0 +1,43 @@
+﻿// Conjure application framework.
+// Copyright (C) Conjure.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Conjure.BlazorKit.Data;
+
+public class DbContextBase : DbContext
+{
+    //
+    // Summary:
+    //     Initializes a new instance of the Microsoft.EntityFrameworkCore.DbContext class
+    //     using the specified options. The
+    //     Microsoft.EntityFrameworkCore.DbContext.OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder)
+    //     method will still be called to allow further configuration of the options.
+    //
+    // Parameters:
+    //   options:
+    //     The options for this context.
+    public DbContextBase(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        // As per:
+        //  https://docs.microsoft.com/en-us/ef/core/change-tracking/change-detection#change-tracking-proxies
+        optionsBuilder.UseChangeTrackingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // By default UseChangeTrackingProxies() enables the
+        // ChangingAndChangeNotifications strategy but we rely
+        // on the "Original Values" to support per entity undo
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+    }
+}
